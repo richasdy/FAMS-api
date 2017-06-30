f3e19cea5ef890c6cf66f8c15a7fa254d24697ca
renaldybrada <renaldybrada@gmail.com>
2017-06-30 19:35:23 +0700

    protect route pakai jwt

--- routes/api.php
+++ routes/api.php
@@ -17,32 +17,32 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-//Route::group(['middleware' => 'auth:api'], function () {
+Route::group(['middleware' => 'auth:api'], function () {
 
-Route::get('index-asset','AssetAPI@ListAsset');
-Route::get('cu-page-asset','AssetAPI@PageAsset');
-Route::get('create-asset','AssetAPI@CreateAsset');
-Route::get('update-asset/{id}','AssetAPI@UpdateAsset');
-Route::get('delete-asset/{id}','AssetAPI@DeleteAsset');
+  Route::get('index-asset','AssetAPI@ListAsset');
+  Route::get('cu-page-asset','AssetAPI@PageAsset');
+  Route::get('create-asset','AssetAPI@CreateAsset');
+  Route::get('update-asset/{id}','AssetAPI@UpdateAsset');
+  Route::get('delete-asset/{id}','AssetAPI@DeleteAsset');
 
-Route::get('index-location','LocationAPI@ListLocation');
-Route::get('cu-page-location','LocationAPI@PageLocation');
-Route::get('create-location','LocationAPI@CreateLocation');
-Route::get('update-location/{id}','LocationAPI@UpdateLocation');
-Route::get('delete-location/{id}','LocationAPI@DeleteLocation');
+  Route::get('index-location','LocationAPI@ListLocation');
+  Route::get('cu-page-location','LocationAPI@PageLocation');
+  Route::get('create-location','LocationAPI@CreateLocation');
+  Route::get('update-location/{id}','LocationAPI@UpdateLocation');
+  Route::get('delete-location/{id}','LocationAPI@DeleteLocation');
 
-Route::get('index-type','TypeAPI@ListType');
-Route::get('cu-page-type','TypeAPI@PageType');
-Route::get('create-type','TypeAPI@CreateType');
-Route::get('update-type/{id}','TypeAPI@UpdateType');
-Route::get('delete-type/{id}','TypeAPI@DeleteType');
+  Route::get('index-type','TypeAPI@ListType');
+  Route::get('cu-page-type','TypeAPI@PageType');
+  Route::get('create-type','TypeAPI@CreateType');
+  Route::get('update-type/{id}','TypeAPI@UpdateType');
+  Route::get('delete-type/{id}','TypeAPI@DeleteType');
 
-Route::get('index-type-detail','TypeDetailAPI@ListTypeDetail');
-Route::get('cu-page-type-detail','TypeDetailAPI@PageTypeDetail');
-Route::get('create-type-detail','TypeDetailAPI@CreateTypeDetail');
-Route::get('update-type-detail/{id}','TypeDetailAPI@UpdateTypeDetail');
-Route::get('delete-type-detail/{id}','TypeDetailAPI@DeleteTypeDetail');
+  Route::get('index-type-detail','TypeDetailAPI@ListTypeDetail');
+  Route::get('cu-page-type-detail','TypeDetailAPI@PageTypeDetail');
+  Route::get('create-type-detail','TypeDetailAPI@CreateTypeDetail');
+  Route::get('update-type-detail/{id}','TypeDetailAPI@UpdateTypeDetail');
+  Route::get('delete-type-detail/{id}','TypeDetailAPI@DeleteTypeDetail');
 
-Route::get('create-user','UserController@createUser');
+  Route::get('create-user','UserController@createUser');
 
-//});
+});
